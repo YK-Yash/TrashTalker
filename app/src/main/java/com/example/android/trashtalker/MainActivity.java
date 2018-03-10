b8db818458d1916b73f989d26c120cd416063349
YK-Yash <yashkumar1998@gmail.com>
2018-03-11 02:47:25 +0530

    Added Fragments

--- app/src/main/java/com/example/android/trashtalker/MainActivity.java
+++ app/src/main/java/com/example/android/trashtalker/MainActivity.java
@@ -3,32 +3,20 @@ package com.example.android.trashtalker;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.example.android.trashtalker.Fragments.Home_page_fragment;
+import com.example.android.trashtalker.Fragments.SensorReadingsFragment;
 import com.gkravas.meterview.MeterView;
 
-import java.util.Random;
-
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-import com.gkravas.meterview.MeterView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
-
-import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
-import rx.functions.Func1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private MeterView meterView;
     FirebaseDatabase db;
     DatabaseReference reference;
-    int sensor_reading;
+    private SectionsStatePagerAdapter mSectionsStatePagerAdapter;
+    private ViewPager mViewPager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -45,13 +34,12 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    setViewPager(0);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    setViewPager(1);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -63,8 +51,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mSectionsStatePagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+
+        mViewPager = (ViewPager) findViewById(R.id.vp_pages);
+        //setup the pager
+        setupViewPager(mViewPager);
+
         mTextMessage = (TextView) findViewById(R.id.message);
-        meterView = findViewById(R.id.meter_view);
+//        meterView = findViewById(R.id.meter_view);
 
         db = FirebaseDatabase.getInstance();
         reference = db.getReferenceFromUrl("https://myapplication-d680d.firebaseio.com/Gas_Readings/MQ135");
@@ -72,13 +66,13 @@ public class MainActivity extends AppCompatActivity {
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                try {
-                    sensor_reading = dataSnapshot.getValue(Integer.class);
-                    //int reading = Integer.valueOf(sensor_reading);
-                    meterView.setValue(sensor_reading);
-                }catch (Exception e){
-                    meterView.setValue(20);
-                }
+//                try {
+////                    sensor_reading = dataSnapshot.getValue(Integer.class);
+//                    //int reading = Integer.valueOf(sensor_reading);
+////                    meterView.setValue(sensor_reading);
+//                }catch (Exception e){
+//                    meterView.setValue(20);
+//                }
             }
 
             @Override
@@ -89,30 +83,26 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-//        final float min = 0;
-//        final float max = 54;
-//        final Random random = new Random();
-//
-//        Observable.interval(500, TimeUnit.MILLISECONDS)
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .map(new Func1<Long, Float>() {
-//                    @Override
-//                    public Float call(Long aLong) {
-//                        return min + (random.nextFloat() * (max - min));
-//                    }
-//                })
-//                .subscribe(new Action1<Float>() {
-//                    @Override
-//                    public void call(Float value) {
-//                        meterView.setValue(value);
-//                    }
-//                });
-
     }
+
+    private void setupViewPager(ViewPager viewPager){
+        SectionsStatePagerAdapter adapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Home_page_fragment(), "Home Page");
+        adapter.addFragment(new SensorReadingsFragment(), "SensorReadings");
+//        adapter.addFragment(new Fragment3(), "Fragment3");
+        viewPager.setAdapter(adapter);
+    }
+
+    public void setViewPager(int fragmentNumber){
+        mViewPager.setCurrentItem(fragmentNumber);
+    }
+
 }
